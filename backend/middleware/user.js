169370b91e69795330e2913cbd3eab1e47e6be5a
ChhaviKukreja <chhavikukreja19@gmail.com>
2025-03-20 23:11:04 +0530

    middleware

---
+++ backend/middleware/user.js
@@ -0,0 +1,27 @@
+const { User }=require("../db");
+const jwt=require("jsonwebtoken");
+const { JWT_SECRET }=require("../config");
+
+function userMiddleware(req,res,next){
+    try{
+        const token=req.headers.authorization;
+        const words=token.split(" ");
+        const jwtToken=words[1];
+
+        const decodedValue= jwt.verify(jwtToken,JWT_SECRET);
+        if (decodedValue.email) {
+            req.email = decodedValue.email; // Set username in request object
+            next();
+        }
+        else{
+            res.status(403).json({
+                msg:"You are not authenticated"
+            })
+        }
+    } catch(e){
+        res.json({
+            msg:"Incorrect inputs"
+        })
+    }  
+}
+module.exports=userMiddleware;
